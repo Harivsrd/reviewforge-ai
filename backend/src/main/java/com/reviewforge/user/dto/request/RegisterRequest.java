31bce6a893ff1fdda1e6f9c5ad9e9499a63542cb
Harivsrd <harivsrdesineedi@gmail.com>
2026-07-05 11:19:55 +0530

    feat(user): add repository and registration DTOs

---
+++ backend/src/main/java/com/reviewforge/user/dto/request/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.reviewforge.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest (
+    
+    @NotBlank(message = "Full name is required")
+    String funnName,
+
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email is required")
+    String email,
+
+    @Size(min = 8, message = "Password must contain at least 8 characters")
+    String password
+ 
+) {}
