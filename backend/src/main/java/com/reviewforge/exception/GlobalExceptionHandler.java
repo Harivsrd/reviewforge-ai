a38b69bd82b06b3d99b9eba3040dafad442e7a10
Harivsrd <harivsrdesineedi@gmail.com>
2026-07-08 00:13:34 +0530

    feat(exception): add global exception handing

--- backend/src/main/java/com/reviewforge/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/reviewforge/exception/GlobalExceptionHandler.java
@@ -1,5 +1,30 @@
 package com.reviewforge.exception;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.reviewforge.common.response.ErrorResponse;
+import com.reviewforge.user.exception.EmailAlreadyExistsException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExists(
+            EmailAlreadyExistsException ex) {
+
+        ErrorResponse response = new ErrorResponse(
+                false,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(response);
+    }
 }
