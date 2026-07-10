3b6348f06699179d0ede41d04c13a80ee8191352
Harivsrd <harivsrdesineedi@gmail.com>
2026-07-11 04:42:01 +0530

    feat(jwt): implement JWT authentication filter stage 1

--- backend/src/main/java/com/reviewforge/security/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/reviewforge/security/jwt/JwtAuthenticationFilter.java
@@ -1,5 +1,46 @@
 package com.reviewforge.security.jwt;
 
-public class JwtAuthenticationFilter {
-    
+import java.io.IOException;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.reviewforge.security.userdetails.CustomUserDetailsService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String jwt = authHeader.substring(7);
+         
+        String username = jwtService.extractUsername(jwt);
+
+        UserDetails userDetails =
+            userDetailsService.loadUserByUsername(username);
+
+        filterChain.doFilter(request, response);
+    }
 }
--- backend/src/main/java/com/reviewforge/security/userdetails/CustomUserDetailService.java
+++
@@ -1,5 +0,0 @@
-package com.reviewforge.security.userdetails;
-
-public class CustomUserDetailService {
-    
-}
